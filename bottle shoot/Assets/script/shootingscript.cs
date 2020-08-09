cbbe85a0084c45c275490bcf4c7a3a432e281b63
Ravik27280 <47824408+Ravik27280@users.noreply.github.com>
2020-08-09 15:34:08 +0530

    Complete
    We upload our complete game.

---
+++ bottle shoot/Assets/script/shootingscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootingscript : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject gamecamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gamecamera.transform.position, gamecamera.transform.forward,out hit))
+        {
+            if (hit.transform.name == "bottle")
+            {
+                Destroy(hit.transform.gameObject);
+                GameObject go = Instantiate(explosion, hit.point, Quaternion.LookRotation(hit.normal)); 
+            }
+        }
+    }
+
+}
